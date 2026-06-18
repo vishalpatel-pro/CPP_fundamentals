dfa4847d057dda764a051b11a5673cf7027e0b43
vishalpatel-pro <vishalp9166@gmail.com>
2026-06-18 11:36:37 +0530

    Add files via upload

---
+++ Assignment 1/area.cpp
@@ -0,0 +1,11 @@
+//  Write a program to calculate the area of a rectangle.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float length, width;
+    cin >> length >> width;
+    cout << "Area = " << length * width;
+    return 0;
+}
---
+++ Assignment 1/hello_world.cpp
@@ -0,0 +1,9 @@
+// Write a program to print "Hello World".
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World";
+    return 0;
+}
